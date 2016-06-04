6c079d2f129c475bf42c6649db242c7f330e38f7
Zobia Arshad <saimaars@hotmail.com>
2016-06-04 03:00:51 +0000

    Initial commit

--- Ibitday/routes/index.js
+++ Ibitday/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) { 'use strict';
-  res.render('index', { title: 'Ibitday' });
+  res.render('index', { title: 'ibitday' });
+});
+router.get('/:id', function(req, res, next) { 'use strict';
+  res.render(req.params.id, { title: 'ibitday' });
 });
 
 module.exports = router;
